149ad5eeed547fedfe2f814f56c04545ae8e857b
CK Finance (CK) <95890905+ckfinancedefi@users.noreply.github.com>
2024-09-27 00:10:17 +0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
+            href="https://ck.app/">
             Learn, Trade and Earn.
           </Link>
         </div>
