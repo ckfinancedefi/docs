b99436cfee8d87a697243ab477f426e4d3624ed3
CK Finance (CK) <95890905+ckfinancedefi@users.noreply.github.com>
2024-09-28 14:17:58 +0300

    update

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -5,7 +5,7 @@ import styles from './styles.module.css';
 const FeatureList = [
   {
     title: 'Trade Now',
-    Svg: require('@site/static/img/ck-moon.png').default,
+    Svg: require('@site/static/img/ck-moon-b04e80ed66ef962c8d82edf679077718.png').default,
     description: (
       <>
         Trade, earn, and own crypto on the all-in-one multichain DEX.
